22272ba28a16116d6ecd421deb42517d316c0b40
rlegorreta01 <rlegorreta@legosoft.com.mx>
2023-06-15 17:41:33 -0600

    2.0.19

--- ailegorreta-kit-commons/ailegorreta-kit-commons-utils/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-project.group = "chafa"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,6 @@ rootProject.name = "ailegorreta-kit"
 include("ailegorreta-kit-commons-utils")
 project(":ailegorreta-kit-commons-utils").projectDir = file("ailegorreta-kit-commons/ailegorreta-kit-commons-utils")
 
+rootProject.children.forEach {
+    it.name = "chafaxxx"
+}
