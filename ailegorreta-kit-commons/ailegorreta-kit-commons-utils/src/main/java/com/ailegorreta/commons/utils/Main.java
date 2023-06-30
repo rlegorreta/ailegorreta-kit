29ff5e3195cb6d7ccce0fcf9bc948d6e1751d3b7
rlegorreta01 <rlegorreta@legosoft.com.mx>
2023-06-30 12:06:12 -0600

    2.0.32

--- ailegorreta-kit-commons/ailegorreta-kit-commons-utils/src/main/java/com/ailegorreta/commons/utils/Main.java
+++
@@ -1,7 +0,0 @@
-package com.ailegorreta.commons.utils;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello com.ailegorreta.commons.utils!");
-    }
-}
